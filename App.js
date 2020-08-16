6653a9a14c6a9c002b57a3af1f1b6560d71e98c8
Jason Brown <browniefed@gmail.com>
2020-08-16 14:08:18 -0700

    drag stop

--- App.js
+++ App.js
@@ -4,53 +4,61 @@ import Animated, {
   useAnimatedStyle,
   useAnimatedGestureHandler,
   measure,
+  withTiming,
   useAnimatedRef,
+  Easing,
 } from 'react-native-reanimated';
-import {View} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import React from 'react';
 
-const Boop = ({width}) => {
-  const style = useAnimatedStyle(() => {
-    return {
-      width: width.value,
-    };
-  });
-
-  return (
-    <Animated.View
-      style={[
-        {
-          height: 80,
-          backgroundColor: 'black',
-          margin: 30,
-        },
-        style,
-      ]}
-    />
-  );
-};
+const THRESHOLD = 120;
 
 export default function AnimatedStyleUpdateExample(props) {
-  const width = useSharedValue(50);
-  const aref = useAnimatedRef();
+  const position = useSharedValue(0);
+  const dragging = useSharedValue(false);
+  const dimensions = Dimensions.get('window');
 
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, ctx) => {
-      const measuredWidth = measure(aref).width;
-      ctx.width = measuredWidth;
+      ctx.position = position.value;
     },
     onActive: (event, ctx) => {
-      width.value = ctx.width + event.translationX;
+      if (Math.abs(event.translationY) >= 30) {
+        dragging.value = true;
+      }
+      if (dragging.value) {
+        position.value = ctx.position + event.translationY;
+      }
     },
-    onEnd: (_) => {
-      width.value = withSpring(50);
+    onEnd: (event) => {
+      const topValue = -1 * dimensions.height + 120;
+      if (event.translationY < -THRESHOLD && event.translationY < 0) {
+        position.value = withTiming(topValue, {
+          duration: 300,
+        });
+      } else if (event.translationY >= THRESHOLD && event.translationY > 0) {
+        position.value = withSpring(0);
+      } else {
+        if (position.value < -THRESHOLD) {
+          position.value = withTiming(topValue, {
+            duration: 150,
+          });
+        } else {
+          position.value = withSpring(0);
+        }
+      }
+      dragging.value = false;
     },
   });
 
-  const style = useAnimatedStyle(() => {
+  const dragStyle = useAnimatedStyle(() => {
     return {
-      width: width.value,
+      transform: [
+        {
+          translateY: position.value,
+        },
+      ],
     };
   });
 
@@ -62,19 +70,36 @@ export default function AnimatedStyleUpdateExample(props) {
       }}>
       <PanGestureHandler onGestureEvent={gestureHandler}>
         <Animated.View
-          ref={aref}
           style={[
             {
-              height: 80,
+              height: '100%',
               backgroundColor: 'black',
-              margin: 30,
+              width: '100%',
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              position: 'absolute',
+              top: dimensions.height - 80,
+              left: 0,
+              right: 0,
             },
-            style,
-          ]}
-        />
+            dragStyle,
+          ]}>
+          <View
+            style={{
+              alignItems: 'center',
+              padding: 10,
+            }}>
+            <View
+              style={{
+                backgroundColor: '#FFF',
+                width: '25%',
+                height: 5,
+                borderRadius: 5,
+              }}
+            />
+          </View>
+        </Animated.View>
       </PanGestureHandler>
-      <Boop width={width} />
-      <Boop width={width} />
     </View>
   );
 }
